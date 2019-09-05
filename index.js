335bb0060e4626b02584b858c87cc5a7f047a6e3
christianhelms <helmsmedia@gmail.com>
2019-09-05 14:56:00 -0500

    added sanity check

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ require('dotenv').config()
 
 const server = require('./server.js');
 
+server.get("/", (req, res) => {
+    res.send("It's alive!");
+  });  
+
 const port = process.env.PORT;
 
 server.listen(port, () => console.log(`\n API on port ${port} \n`));
